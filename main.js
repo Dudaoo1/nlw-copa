35ab5f46b057871dd84c7bb70b3238cd18646180
Dudaoo1 <eduardosdo123@gmail.com>
2022-11-06 20:34:26 -0300

    projeto finalizado

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-function createGame(formatoDaImg, player1, hour, player2, formatoDaImg2) {
-  console.log(formatoDaImg)
+function createGame(player1, formatoDaImg1, hour, player2, formatoDaImg2) {
   return `
     <li>
-      <img src="./assets/icon-${player1}.${formatoDaImg}" alt="Bandeira do ${player1}" />
+      <img 
+        id="${player1}"
+        src="./assets/icon-${player1}.${formatoDaImg1}" 
+        alt="Bandeira do ${player1}" 
+        title="${player1}"
+      />
+
       <strong>${hour}</strong>
-      <img src="./assets/icon-${player2}.${formatoDaImg2}" alt="Bandeira da ${player2}." />
+
+      <img 
+        id="${player2}" 
+        src="./assets/icon-${player2}.${formatoDaImg2}" 
+        alt="Bandeira da ${player2}." 
+        title="${player2}"
+      />
     </li>
   `
 }
@@ -21,84 +32,121 @@ function createCard(date, day, games) {
 }
 
 const app = (document.querySelector("#app").innerHTML = `
-      <header>
-        <img src="./assets/logo.gif" alt="Logo da NLW" />
-      </header>
-
-      <main id="cards">
-        ${createCard(
-          "20/11",
-          "domingo",
-          createGame("gif", "qatar", "13:00", "ecuador", "gif")
-        )}
-         ${createCard(
-           "21/11",
-           "sexta",
-           createGame("gif", "england", "10:00", "iran", "gif") +
-             createGame("gif", "senegal", "13:00", "netherlands", "gif") +
-             createGame("gif", "usa", "16:00", "wales2", "gif")
-         )}
-
-          ${createCard(
-            "22/11",
-            "sexta",
-            createGame("gif", "argentina", "07:00", "saudiarabia", "gif") +
-              createGame("gif", "denmark", "10:00", "tunisa", "gif") +
-              createGame("gif", "mexico2", "13:00", "poland", "gif") +
-              createGame("gif", "franca", "16:00", "australian2", "gif")
-          )}
-
-           ${createCard(
-             "23/11",
-             "sexta",
-             createGame("gif", "morocco", "07:00", "croatia", "gif") +
-               createGame("gif", "germany", "10:00", "japan", "gif") +
-               createGame("gif", "spain", "13:00", "costarica", "gif") +
-               createGame("gif", "belgium", "16:00", "canada", "gif")
-           )}
-
-        ${createCard(
-          "24/11",
-          "quinta",
-          createGame("svg", "switzerland", "07:00", "cameroon", "svg") +
-            createGame("gif", "franca", "10:00", "south korea", "svg") +
-            createGame("svg", "portugal", "13:00", "ghana", "svg") +
-            createGame("svg", "brazil", "16:00", "serbia", "svg")
-        )}
-
-        ${createCard(
-          "28/11",
-          "segunda",
-          createGame("svg", "brazil", "13:00", "switzerland", "svg") +
-            createGame("svg", "portugal", "16:00", "uruguai", "svg") +
-            createGame("svg", "portugal", "16:00", "uruguai", "svg") +
-            createGame("svg", "portugal", "16:00", "uruguai", "svg")
-        )}
-
-        ${createCard(
-          "02/12",
-          "sexta",
-          createGame("svg", "brazil", "16:00", "cameroon", "svg") +
-            createGame("svg", "brazil", "16:00", "cameroon", "svg") +
-            createGame("svg", "brazil", "16:00", "cameroon", "svg")
-        )}
-
-         ${createCard(
-           "02/12",
-           "sexta",
-           createGame("svg", "brazil", "16:00", "cameroon", "svg") +
-             createGame("svg", "brazil", "16:00", "cameroon", "svg") +
-             createGame("svg", "brazil", "16:00", "cameroon", "svg")
-         )}
-
-          ${createCard(
-            "02/12",
-            "sexta",
-            createGame("svg", "brazil", "16:00", "cameroon", "svg") +
-              createGame("svg", "brazil", "16:00", "cameroon", "svg") +
-              createGame("svg", "brazil", "16:00", "cameroon", "svg")
-          )}
-
+        <header>
+          <img src="./assets/logo5.png" alt="Logo da NLW" />
         
-      </main>
+          <div id="mascote2">
+            <img id="mascote2" src="./assets/mascote2.gif" alt="Logo da NLW" />
+ 
+          </div>
+      
+      </header>
 `)
+
+document.querySelector("#cards").innerHTML =
+  createCard(
+    "20/11",
+    "domingo",
+    createGame("qatar", "gif", "13:00", "ecuador", "gif")
+  ) +
+  createCard(
+    "21/11",
+    "segunda",
+    createGame("england", "gif", "10:00", "iran", "gif") +
+      createGame("senegal", "gif", "13:00", "netherlands", "gif") +
+      createGame("usa", "gif", "16:00", "wales2", "gif")
+  ) +
+  createCard(
+    "22/11",
+    "terça",
+    createGame("argentina", "gif", "07:00", "saudiarabia", "gif") +
+      createGame("denmark", "gif", "10:00", "tunisa", "gif") +
+      createGame("mexico2", "gif", "13:00", "poland", "gif") +
+      createGame("franca", "gif", "16:00", "australian2", "gif")
+  ) +
+  createCard(
+    "23/11",
+    "quarta",
+    createGame("morocco", "gif", "07:00", "croatia", "gif") +
+      createGame("germany", "gif", "10:00", "japan", "gif") +
+      createGame("spain", "gif", "13:00", "costarica", "gif") +
+      createGame("belgium", "gif", "16:00", "canada", "gif")
+  ) +
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "gif", "07:00", "cameroon", "gif") +
+      createGame("uruguay", "gif", "10:00", "southkorea", "gif") +
+      createGame("portugal", "gif", "13:00", "ghana", "gif") +
+      createGame("brazil", "gif", "16:00", "serbia", "gif")
+  ) +
+  createCard(
+    "25/11",
+    "sexta",
+    createGame("wales2", "gif", "07:00", "iran", "gif") +
+      createGame("qatar", "gif", "10:00", "senegal", "gif") +
+      createGame("netherlands", "gif", "13:00", "ecuador", "gif") +
+      createGame("england", "gif", "16:00", "usa", "gif")
+  ) +
+  createCard(
+    "26/11",
+    "sabado",
+    createGame("tunisa", "gif", "07:00", "australian2", "gif") +
+      createGame("poland", "gif", "10:00", "saudiarabia", "gif") +
+      createGame("franca", "gif", "13:00", "denmark", "gif") +
+      createGame("argentina", "gif", "16:00", "mexico2", "gif")
+  ) +
+  createCard(
+    "27/11",
+    "domingo",
+    createGame("japan", "gif", "07:00", "costarica", "gif") +
+      createGame("belgium", "gif", "10:00", "morocco", "gif") +
+      createGame("croatia", "gif", "13:00", "canada", "gif") +
+      createGame("spain", "gif", "16:00", "germany", "gif")
+  ) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("cameroon", "gif", "07:00", "serbia", "gif") +
+      createGame("southkorea", "gif", "10:00", "ghana", "gif") +
+      createGame("brazil", "gif", "13:00", "switzerland", "gif") +
+      createGame("portugal", "gif", "16:00", "uruguay", "gif")
+  ) +
+  createCard(
+    "29/11",
+    "terça",
+    createGame("ecuador", "gif", "07:00", "senegal", "gif") +
+      createGame("netherlands", "gif", "10:00", "qatar", "gif") +
+      createGame("iran", "gif", "13:00", "usa", "gif") +
+      createGame("wales2", "gif", "16:00", "england", "gif")
+  ) +
+  createCard(
+    "30/11",
+    "quarta",
+    createGame("tunisa", "gif", "07:00", "franca", "gif") +
+      createGame("australian2", "gif", "10:00", "denmark", "gif") +
+      createGame("poland", "gif", "13:00", "argentina", "gif") +
+      createGame("saudiarabia", "gif", "16:00", "mexico2", "gif")
+  ) +
+  createCard(
+    "01/12",
+    "quinta",
+    createGame("croatia", "gif", "07:00", "belgium", "gif") +
+      createGame("canada", "gif", "10:00", "morocco", "gif") +
+      createGame("japan", "gif", "13:00", "spain", "gif") +
+      createGame("costarica", "gif", "16:00", "germany", "gif")
+  ) +
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("southkorea", "gif", "07:00", "portugal", "gif") +
+      createGame("ghana", "gif", "10:00", "uruguay", "gif") +
+      createGame("serbia", "gif", "13:00", "switzerland", "gif") +
+      createGame("cameroon", "gif", "16:00", "brazil", "gif")
+  )
+
+// var detectarQualPaisBandeira = document.querySelector("#brazil")
+
+// detectarQualPaisBandeira.addEventListener("mouseover", function (e) {
+//   console.log("teste")
+// })
